eff40f50729a86039e93b189d3e6939484c2fcaa
Osmond32 <saia.giuseppe@live.it>
2026-05-11 13:57:44 +0200

    fix: url finalee

--- Frontend/src/services/utilisateurService.js
+++ Frontend/src/services/utilisateurService.js
@@ -5,7 +5,9 @@
 import axios from 'axios';
 
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL = import.meta.env.MODE === 'production' 
+    ? 'https://project-user-2z02.onrender.com/api' 
+    : 'http://localhost:3000/api';
 
 export const register = async (userData) => {
     try {
